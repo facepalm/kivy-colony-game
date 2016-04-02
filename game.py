892f3db28ff26e661f07cae42c8bf5b525532594
Brad <bbusse@gmail.com>
2016-04-01 23:02:05 -0400

    More realistic star generation

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import util
 import numpy as np
 import planet
+from scipy.stats import wald
 
 class Universe(object):    
     def __init__(self):
@@ -9,11 +10,13 @@ class Universe(object):
     def generate_system(self,system_type='Generic'):
     
         self.system_distribution = util.getWackyDist(total_mass = 1E29, objects = 20, wacky_facty = 0.5)
-                        
-        primary_star_mass = 2E30 + 1E29*np.random.randn() #self.system_distribution.max()
+                            
+        primary_star_mass = wald.rvs(loc=0.2, scale=2, size=1)[0]
+        
         #self.system_distribution = self.system_distribution[self.system_distribution != primary_star_mass]
         
-        self.primary = planet.Star(mass=primary_star_mass)
+        self.primary = planet.Star(solar_masses=primary_star_mass)
+        print self.primary.info()
         
         num_orbits = np.random.randint(8,18)
         orbits = [pow(10,1.5*x)- 0.6 for x in np.random.random(num_orbits)]
@@ -28,6 +31,7 @@ class Universe(object):
             print mass, orbits[i]
                 
             #initialize planet, extend list (might be a list of asteroids instead)    
-            self.planets.extend([])
+            newp = planet.generate_planet(mass,self.primary,orbits[i])
+            self.planets.extend(newp)
             
         #habitable zone world            
--- planet.py
+++ planet.py
@@ -3,12 +3,17 @@ import util
 import logging
 import random
 
-
+def generate_planet(mass,sun,orbit):
+    p = Planet(mass,sun,orbit)
+    #eventually walk through planetary history - greenhouse events, asteroidation, etc
+    return [p]
 
 class Planet(object):
     def __init__(self,mass=None,sun=None,orbit=None,name=None, logger=None):
         self.mass = mass if mass else 1E8*random.random()
         self.name = name if name else util.planet_name(self)
+        self.sun=sun
+        self.orbit = orbit
         if logger:
             self.logger = logging.getLogger(logger.name + '.' + self.name)
         else: 
@@ -30,23 +35,74 @@ class Planet(object):
         self.initialize_sites()
         
     def initialize_sites(self):
-        self.sites = 6 if self.type == 'Planet' else 2 if self.type == 'Dwarf' else 1 if self.type == 'Planetoid' else 0
+        self.sites = 6 if self.type == 'Planet' else 2 if self.type == 'Dwarf Planet' else 1 if self.type == 'Planetoid' else 0
         self.orbits = 1
-        print self.sites, self.orbits
+        #print self.sites, self.orbits
     
 class Star(object):
-    def __init__(self, mass, name=None, logger=None):
-        self.mass = mass
+    def __init__(self, solar_masses, name=None, logger=None):
+        self.solar_masses = solar_masses
+        self.mass = self.solar_masses*2E30
         self.name = name if name else util.star_name(self)
         if logger:
             self.logger = logging.getLogger(logger.name + '.' + self.name)
         else: 
             self.logger = logging.getLogger(util.generic_logger.name + '.' + self.name)
     
-        if self.mass > 1E31: 
-            self.type = 'Super' #TODO do this right
-        else:
-            self.type = 'Small'
+        #current assumption: main sequence star.  May want to simulate lifetimes and do giants in the future
+        # reference: https://en.wikipedia.org/wiki/Stellar_classification
+        if self.solar_masses < 0.5: 
+            self.type = 'M'
+            self.radius = 0.7
+            self.luminosity = 0.08
+            self.color = 'Red'
+        elif self.solar_masses < 0.8: 
+            frac = (self.solar_masses - 0.5)/0.3
+            self.type = 'K'
+            self.radius = 0.7 + 0.26*frac
+            self.luminosity = 0.08 + 0.52*frac
+            self.color = 'Orange'
+        elif self.solar_masses < 1.04: 
+            frac = (self.solar_masses - 0.8)/0.24
+            self.type = 'G'
+            self.radius = 0.96 + (1.15-0.96)*frac
+            self.luminosity = 0.6 + (1.5 - 0.6)*frac
+            self.color = 'Yellow'
+        elif self.solar_masses < 1.4: 
+            frac = (self.solar_masses - 1.04)/0.36
+            self.type = 'F'
+            self.radius = 1.15 + (1.4-1.15)*frac
+            self.luminosity = 1.5 + (5 - 1.5)*frac
+            self.color = 'Yellow-White'
+        elif self.solar_masses < 2.1: 
+            frac = (self.solar_masses - 1.4)/0.7
+            self.type = 'A'
+            self.radius = 1.4 + (1.8-1.4)*frac
+            self.luminosity = 5 + (25 - 5)*frac
+            self.color = 'White'   
+        elif self.solar_masses < 16: 
+            frac = (self.solar_masses - 2.1)/(16-2.1)
+            self.type = 'B'
+            self.radius = 1.8 + (6.6-1.8)*frac
+            self.luminosity = 25 + (30000 - 25)*frac
+            self.color = 'Blue-White'  
+        else: # self.solar_masses > 16: 
+            frac = (self.solar_masses - 2.1)/(16-2.1)
+            self.type = 'O'
+            self.radius = 8 #arbitrary
+            self.luminosity = 50000 #arbitrary
+            self.color = 'Blue'     
+        
+        self.habitable_start = 0.75 * pow( self.luminosity ,0.5)
+        self.habitable_end = 1.4 * pow( self.luminosity ,0.5)
+        
+        self.snow_line = 3 * pow( self.luminosity ,0.5)
+
+    def info(self):
+        out = self.type+'-type star, with mass of %.2f' % self.solar_masses + ' and luminosity of %.2f' % self.luminosity
+        out += ', Habitable zone between %.2f' % self.habitable_start +' and %.2f' % self.habitable_end
+        out += ', Snow line at %.2f' % self.snow_line
+        return out
         
     
 if __name__ == "__main__":        
