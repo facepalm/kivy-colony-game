0a37e6e02f0c061590aa3b4d6946074e98edf1dd
Brad <bbusse@gmail.com>
2016-04-10 13:19:33 -0400

    Fix minor bug in resources

--- planetresources.py
+++ planetresources.py
@@ -11,7 +11,7 @@ raw_dist = np.array([ 1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001])
 class PlanetResources(object):
     def __init__(self,planet):
         self.planet=planet
-        self.raw = np.zeros((10,1),dtype=np.float32)
+        self.raw = np.zeros((1,10),dtype=np.float32)
         self.atmo = 0
         self.atmo_type = 'None'
         
@@ -19,7 +19,7 @@ class PlanetResources(object):
         
         if planet.type == 'Planetoid':
             #the most interesting type, planetoids split into VERY diverse categories
-            self.raw = np.random.random((10,1))
+            self.raw = np.random.random(10)
             planet.subtype = random.choice('CSXc')
             if planet.subtype == 'C': #carbonaceous, from crust of former planet
                 self.raw = np.multiply(self.raw,np.array([0.01,0.1,1,1,0.01,1,1,0.01,0.01,1]))
@@ -30,18 +30,18 @@ class PlanetResources(object):
             else: #c type, chondrites, leave everything as it is
                 pass
             if planet.orbit < planet.primary.snow_line:
-                self.raw *= np.array([1,1,0.001,1,1,1,0.01,1,1,0.01])
+                self.raw = np.multiply(self.raw,np.array([1,1,0.001,1,1,1,0.01,1,1,0.01]))
                 
         elif planet.type == 'Dwarf planet':
             #dwarf planets have raw resources, but no atmo
-            self.raw = np.random.random((10,1))
+            self.raw = np.random.random(10)
             if planet.orbit < planet.primary.snow_line:
                 self.raw[2] /= 100 #most hydrates boiled off
                 self.raw[9] /= 100 #most noble gasses likewise
             #if planet.
         elif planet.type == 'Planet':
             #planets have raw resources, and atmo which can (sometimes) protect them
-            self.raw = np.random.random((10,1))
+            self.raw = np.random.random(10)
             self.atmo = random.random() #TODO scale density to planet mass
             self.atmo_type = random.choice(['Organic','Corrosive','Hydrate'])
             if planet.orbit < planet.primary.snow_line and self.atmo < 0.1:
