35498325ee979fccfc10f46dc054ae7b07bae3ce
Brad <bbusse@gmail.com>
2016-04-14 22:49:14 -0400

    Adds potential mine sites; display to site info panel

--- planetsite.py
+++ planetsite.py
@@ -9,11 +9,14 @@ class Site(object):
         self.planet = planet
         self.location = location
         
-        self.resources = np.zeros((1,10),dtype='float32') if 'Orbit' in self.location else np.multiply(2*np.random.random(10),self.planet.resources.raw)
+        self.resources = np.zeros((1,10),dtype='float32').squeeze() if 'Orbit' in self.location else np.multiply(2*np.random.random(10),self.planet.resources.raw)
         self.effective_resources = np.multiply(self.resources, self.planet.resources.raw_dist)
         if self.effective_resources.sum() > 0: self.effective_resources /= sum(self.effective_resources)
         
-        print self.planet.name, self.location, self.effective_resources
+        self.mine_init = np.random.random(10)
+        self.mine = self.mine_init < (self.resources/2)
+        
+        print self.planet.name, self.location, self.effective_resources, self.mine
         
     def small_view(self):
         return siteview.SiteEntrySmall(site=self)        
--- siteview.py
+++ siteview.py
@@ -4,6 +4,7 @@ kivy.require('1.9.2')
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
 
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.graphics import Line, Color, Rectangle
@@ -21,15 +22,52 @@ import globalvars
 import matplotlib.pyplot as plt
 
 entry_small_kv = '''
+<MineLoc@Widget>:
+    size: 25,25
+    pos_hint: {'center_x': .5, 'center_y': .5}
+    canvas:
+        BorderImage:
+            border: 5,5,5,5
+            source: 'images/kivy/button_white.png'
+            pos: self.center_x-10, self.center_y - 10
+            size: 20,20
+
 <SiteEntrySmall>:
     orientation: 'horizontal'
+    id: entry
     
     canvas:
         Color:
             rgb: (0.05, 0.05, 0.15)
         Rectangle:
             size: self.size[0], self.size[1]*0.9
-            pos: self.pos      
+            pos: self.pos     
+            
+    Label:
+        text: entry.site.location    
+    
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[0] else 1.0
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[1] else 1.0
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[2] else 1.0
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[3] else 1.0        
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[4] else 1.0                    
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[5] else 1.0        
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[6] else 1.0                        
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[7] else 1.0        
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[8] else 1.0            
+    MineLoc:
+        opacity: 0.1 if not entry.site.mine[9] else 1.0         
+    Label:
+        text: "End"       
 '''
 
 
@@ -39,11 +77,12 @@ class SiteEntrySmall(BoxLayout):
     def __init__(self, **kwargs):
         self.site = kwargs['site']
         super(SiteEntrySmall, self).__init__(**kwargs)                
-
+        
         
     def on_touch_down(self, touch):
         touch.push()
         touch.apply_transform_2d(self.to_widget)
         touched = self.collide_point(*touch.pos)
         touch.pop()
-                  
+        if touched:
+            print self.site.location, self.site.resources, self.site.effective_resources
